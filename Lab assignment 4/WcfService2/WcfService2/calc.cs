2c7cf02e6e52255a9b38e89373f0a014978fbe55
tmrhc <tmrhc@mail.umkc.edu>
2013-09-24 00:02:53 -0500

    LAB assignment 4

---
+++ Lab assignment 4/WcfService2/WcfService2/calc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface calc
+    {
+
+        [OperationContract]
+    
+    [WebInvoke(Method = "GET", ResponseFormat= WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "Addition/{a}/{b}" )]
+
+        String Addition(string a,string b); 
+
+        [OperationContract]
+     
+    [WebInvoke(Method = "GET", ResponseFormat= WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "Subtraction/{a}/{b}")]
+
+        String Subtraction(string a, string b);
+
+        [OperationContract]
+     
+    [WebInvoke(Method = "GET", ResponseFormat= WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "Multiplication/{a}/{b}")]
+
+        String Multiplication(string a, string b);
+        
+        [OperationContract]
+     
+    [WebInvoke(Method = "GET", ResponseFormat= WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "Division/{a}/{b}")]
+
+        String Division(string a, string b);
+
+        // TODO: Add your service operations here
+    }
+}
---
+++ Lab assignment 4/WcfService2/WcfService2/calc.svc.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : calc
+    {
+        public string Addition(string a, string b)
+        {
+            int a1 = Convert.ToInt32(a);
+            int b1 = Convert.ToInt32(b);
+            int r1 = a1 + b1;
+            string r= Convert.ToString(r1);
+            return r;
+
+        }
+
+        public string Subtraction(string a, string b)
+        {
+            int a1 = Convert.ToInt32(a);
+            int b1 = Convert.ToInt32(b);
+            int r1 = a1 - b1;
+            string r = Convert.ToString(r1);
+            return r;
+        }
+
+        public string Multiplication(string a, string b)
+        {
+            int a1 = Convert.ToInt32(a);
+            int b1 = Convert.ToInt32(b);
+            int r1 = a1 * b1;
+            string r = Convert.ToString(r1);
+            return r;
+
+        }
+        
+        public string Division(string a, string b)
+        {
+            int a1 = Convert.ToInt32(a);
+            int b1 = Convert.ToInt32(b);
+            int r1 = a1 / b1;
+            string r = Convert.ToString(r1);
+            return r;
+
+        }
+
+           }
+
+
+}
