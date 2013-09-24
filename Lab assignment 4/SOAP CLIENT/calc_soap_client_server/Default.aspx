dd0dbfb173dad6a3d9fed5f8d6b9e7feae1b7f8e
tmrhc <tmrhc@mail.umkc.edu>
2013-09-24 14:58:21 -0500

    Lab assignment 4 updated

---
+++ Lab assignment 4/SOAP CLIENT/calc_soap_client_server/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Calculator SOAP Client Service</div>
+        <p>
+            a&nbsp;&nbsp;
+            <asp:TextBox ID="op1" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+        </p>
+        b&nbsp;&nbsp;
+        <asp:TextBox ID="op2" runat="server" OnTextChanged="TextBox3_TextChanged"></asp:TextBox>
+        <br />
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Addition" />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Subtraction" />
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Multiplication" />
+            <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Division" />
+        </p>
+        <p>
+            Result&nbsp;&nbsp;
+            <asp:TextBox ID="op3" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Lab assignment 4/SOAP CLIENT/calc_soap_client_server/Default.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(op1.Text);
+        int b = Convert.ToInt32(op2.Text);
+        ServiceReference1.CalculatorserviceSoapClient p = new ServiceReference1.CalculatorserviceSoapClient();
+        int result= p.Addition(a, b);
+        op3.Text = result.ToString();
+
+            }
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void TextBox3_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(op1.Text);
+        int b = Convert.ToInt32(op2.Text);
+        ServiceReference1.CalculatorserviceSoapClient p = new ServiceReference1.CalculatorserviceSoapClient();
+        int result = p.Subtraction(a, b);
+        op3.Text = result.ToString();
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+
+        int a = Convert.ToInt32(op1.Text);
+        int b = Convert.ToInt32(op2.Text);
+        ServiceReference1.CalculatorserviceSoapClient p = new ServiceReference1.CalculatorserviceSoapClient();
+        int result = p.Multiplication(a, b);
+        op3.Text = result.ToString();
+    }
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(op1.Text);
+        int b = Convert.ToInt32(op2.Text);
+        ServiceReference1.CalculatorserviceSoapClient p = new ServiceReference1.CalculatorserviceSoapClient();
+        int result = p.Division(a, b);
+        op3.Text = result.ToString();
+    }
+}
